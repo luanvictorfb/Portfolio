fee9be4d92d4b597384ddcd9618b0af6943b7740
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-01-27 03:25:12 -0300

    26/01

--- portfolio/Portfólio.js
+++ portfolio/Portfólio.js
@@ -1,6 +1,8 @@
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('nav-mobile')
 let fundo = document.getElementById('fundo')
+let div = document.querySelectorAll('div')
+let nav = document.querySelectorAll('header a')
 
 btnMenu.addEventListener('click',()=>{
     menu.classList.add('abrir-menu')
@@ -13,3 +15,22 @@ menu.addEventListener('click',()=>{
 fundo.addEventListener('click',()=>{
     menu.classList.remove('abrir-menu')
 })
+
+
+
+window.onscroll=()=>{
+    div.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop-150;
+        let heigth=sec.offsetHeight;
+        let id=sec.getAttribute('id')
+        if(top>offset && top<offset + heigth ){
+            nav.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header menu a[href*='+id+']').classList.add('active')
+            })
+        }
+
+
+    })
+}
